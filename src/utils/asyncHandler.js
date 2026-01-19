a3d98488c5bad2f0c06b33bec9786febe0cf8399
RAQIB404 <raqibkhan2332@gmail.com>
2026-01-20 00:04:06 +0530

    add user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return(req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err))
     }
@@ -14,7 +14,7 @@ export {asyncHandler}
 //     try{
 //         await fn(req, res, next)
 //     }catch(error){
-//         res.status(err.code || 500).json({
+//         res.status(error.code || 500).json({
 //             success: false,
 //             message: err.message
 //         })
